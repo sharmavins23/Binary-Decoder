4a10bbaff17318816d04a6e06c626c9f6bee0eaf
Vineeth Sharma <sharmavins23@gmail.com>
2020-12-23 20:00:42 -0500

    flipped bits from previous mishap

--- Binary Decoder.src/modules/SevenSegmentController.v
+++ Binary Decoder.src/modules/SevenSegmentController.v
@@ -26,24 +26,24 @@ module SevenSegmentController (
         // Set enable as a concatenation with counter
         sevenSegmentEnable = {2'b11, ~counter, counter};
 
-        // Hex data out courtesy of @AndrewJonesPSU
+        // Hex data segment output
         case(dataNibble)
-            4'b0000: sevenSegmentData <= 8'b10001000; // 0
-            4'b0001: sevenSegmentData <= 8'b11101011; // 1
-            4'b0010: sevenSegmentData <= 8'b01001100; // 2
-            4'b0011: sevenSegmentData <= 8'b01001001; // 3
-            4'b0100: sevenSegmentData <= 8'b00101011; // 4
-            4'b0101: sevenSegmentData <= 8'b00011001; // 5
+            4'b0000: sevenSegmentData <= 8'b00010001; // 0
+            4'b0001: sevenSegmentData <= 8'b11010111; // 1
+            4'b0010: sevenSegmentData <= 8'b00110010; // 2
+            4'b0011: sevenSegmentData <= 8'b10010010; // 3
+            4'b0100: sevenSegmentData <= 8'b11010100; // 4
+            4'b0101: sevenSegmentData <= 8'b10011000; // 5
             4'b0110: sevenSegmentData <= 8'b00011000; // 6
-            4'b0111: sevenSegmentData <= 8'b11001011; // 7
-            4'b1000: sevenSegmentData <= 8'b00001000; // 8
-            4'b1001: sevenSegmentData <= 8'b00001001; // 9
-            4'b1010: sevenSegmentData <= 8'b00001010; // A
-            4'b1011: sevenSegmentData <= 8'b00111000; // b
-            4'b1100: sevenSegmentData <= 8'b10011100; // C
-            4'b1101: sevenSegmentData <= 8'b01101000; // d
-            4'b1110: sevenSegmentData <= 8'b00011100; // E
-            4'b1111: sevenSegmentData <= 8'b00011110; // F
+            4'b0111: sevenSegmentData <= 8'b11010011; // 7
+            4'b1000: sevenSegmentData <= 8'b00010000; // 8
+            4'b1001: sevenSegmentData <= 8'b10010000; // 9
+            4'b1010: sevenSegmentData <= 8'b01010000; // A
+            4'b1011: sevenSegmentData <= 8'b00011100; // b
+            4'b1100: sevenSegmentData <= 8'b00111001; // C
+            4'b1101: sevenSegmentData <= 8'b00010110; // d
+            4'b1110: sevenSegmentData <= 8'b00111000; // E
+            4'b1111: sevenSegmentData <= 8'b01111000; // F
         endcase
     end
 endmodule
